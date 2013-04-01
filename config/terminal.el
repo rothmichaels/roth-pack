b2fd46124b9b265dc23e59df7cb5ddb03c549372
Roth Michaels <roth@rothmichaels.us>
2013-04-01 12:41:23 -0400

    term-mode experiments

--- config/terminal.el
+++ config/terminal.el
@@ -1,8 +1,14 @@
 ;; fix the shell
-(window-number-mode -1)
+;(window-number-mode -1)
 (window-number-meta-mode -1)
 
+(add-hook 'term-mode-hook
+          '(lambda ()
+             (yas-minor-mode 0)
+             (define-key term-raw-map "\C-y" 'term-send-raw)))
 
 ;;; key bindings
 
-(global-set-key [?\C-c ?\C-r] 'eval-region) ; this should maybe not be global
+;(global-set-key [?\C-c ?\C-r] 'eval-region)
+                                        ; this should maybe not be global
+
