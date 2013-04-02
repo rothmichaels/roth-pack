1fe2c77da8d8a4e2f5f4e395d0449cac855e5e1b
Roth Michaels <roth@rothmichaels.us>
2013-04-02 19:03:54 -0400

    New terminal experiments

--- config/terminal.el
+++ config/terminal.el
@@ -1,11 +1,17 @@
 ;; fix the shell
-;(window-number-mode -1)
-(window-number-meta-mode -1)
+(window-number-mode -1)
+(window-number-meta-mode 1)
 
-(add-hook 'term-mode-hook
-          '(lambda ()
-             (yas-minor-mode 0)
-             (define-key term-raw-map "\C-y" 'term-send-raw)))
+(setq term-mode-hook nil)
+
+(defun roth-term-mode-hook ()
+  (goto-address-mode)
+  (yas-minor-mode 0))
+
+(add-hook 'term-mode-hook 'roth-term-mode-hook)
+
+;(define-key term-raw-map "\C-y" 'term-send-raw)
+
 
 ;;; key bindings
 
