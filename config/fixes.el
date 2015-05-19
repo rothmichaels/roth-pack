ff5313e3eae6151a30f5261d1ae67f3514cae484
Roth Michaels <roth@rothmichaels.us>
2015-05-19 19:24:09 -0400

    Removed fixes.el.
    Setting up pull requests to cleanup monkey-patch implementation so these
    fixes are no longer needed.

--- config/fixes.el
+++
@@ -1,3 +0,0 @@
-;; remove the monkeypatch basic-save-buffer setup by foundation-pack
-
-(defalias 'basic-save-buffer 'live-mp-new-basic-save-buffer)
--- init.el
+++ init.el
@@ -1,6 +1,5 @@
 ;;; global adjustments
 
-;(live-load-config-file "fixes.el")
 (live-load-config-file "global-modes.el")
 (live-load-config-file "terminal.el")
 
